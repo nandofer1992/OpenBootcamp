254265e469b7ca33cf4d8e924e495c1dc53ba938
Fernando_Simon <nandofersimon@gmail.com>
2022-08-30 19:55:23 -0300

    OpenBootcamp

---
+++ Odin.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javax.swing.*;
+
+public class Odin {
+    public static void main(String[] args) {
+
+        int prueba1 = 3000000;
+        double prueba2 = 2.1d;
+        long prueba3 = 3500000l;
+        String prueba4 = "vamos por todo";
+        boolean prueba5 = true;
+
+        System.out.println(prueba1);
+        System.out.println(prueba2);
+        System.out.println(prueba3);
+        System.out.println(prueba4);
+        System.out.println(prueba5);
+
+
+
+    }
+}
